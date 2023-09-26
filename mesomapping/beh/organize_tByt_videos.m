c43d3b20f232cde26d2f68f93c1d0588794ea69b
jp3025 <jp3025@princeton.edu>
2023-09-26 15:19:46 -0400

    update from my laptop

--- mesomapping/beh/organize_tByt_videos.m
+++ mesomapping/beh/organize_tByt_videos.m
@@ -10,114 +10,51 @@ if isempty(filePath_raw_vid)
     filePath_raw_vid = uigetdir(filePath); 
 end
 vidFiles = GrabFiles_sort_trials('behvid', 0, {filePath_raw_vid}); 
+if numel(tbytDat) == numel(vidFiles)    
+    vidTrialC = cellfun(@(a) regexp(a, '_([0-9.]+)\.', 'tokens'), vidFiles, 'un', 0); 
+    vidTrial = cell2mat(cellfun(@(c) str2double(c{1}{1}), vidTrialC, 'un', 0));
+    [~, vidI] = sort(vidTrial); 
+    vidNameSorted = vidFiles(vidI); 
+    [tbytDat(:).vidFile] = deal(vidNameSorted{:}); 
+else
+    warning('The number of raw video files does not match with the number of trials!')
+end    
+disp("Completed organizing the raw video files!")
 
 % DLC bodypart coordinate csv files
 filePath_cropped_vid = findFolderWithString(filePath, '_vid_cropped');
 csvFiles = dir(fullfile(filePath_cropped_vid, 'behvid*.csv'));
-
+% csv files
 if numel(tbytDat) == numel(csvFiles)
     csvNames = {csvFiles.name}; 
-    csvTimeC = cellfun(@(a) hmsToDateTime(a(8:15)), csvNames, 'un', 0); 
+    csvTimeTokenC = cellfun(@(a) regexp(a, '(\d{2}_\d{2}_\d{2})', 'tokens'), csvNames, 'un', 0); 
+    csvTimeC  = cellfun(@(c) hmsToDateTime(c{1}{1}), csvTimeTokenC, 'un', 0); 
     [~, csvTimeI] = sort([csvTimeC{:}]); % to ensure the csv files are in correct timely order
     csvNamesSorted = csvNames(csvTimeI); 
+    csvNamesSorted = cellfun(@(a) fullfile(filePath_cropped_vid, a), csvNamesSorted, 'un', 0); 
     [tbytDat(:).csvFile] = deal(csvNamesSorted{:}); 
 else
     warning('The number of csv files does not match with the number of trials!') % If some videos are missing each videoes need to be assigned using the trial indices in their names (TO DO)
 end
-
-%% parse 
-ellipse_areaC = cell(numel(tbytDat), 1); 
-for t = 1:numel(tbytDat)
-
-    csvTab = readDlcCsv(fullfile(filePath_cropped_vid, tbytDat(t).csvFile)); 
-    csvTab = removevars(csvTab, 'bodyparts_coords'); % drop redundant info
-    newVarNames = {'L_x', 'L_y', 'L_like', ...
-               'LD_x', 'LD_y', 'LD_like', ... 
-               'D_x', 'D_y', 'D_like', ... 
-               'DR_x', 'DR_y', 'DR_like', ... 
-               'R_x', 'R_y', 'R_like', ... 
-               'RV_x', 'RV_y', 'RV_like', ... 
-               'V_x', 'V_y', 'V_like', ... 
-               'VL_x', 'VL_y', 'VL_like'};
-    csvTab.Properties.VariableNames = newVarNames;
+disp("Completed organizing the csv DLC coordinate files!")
 
 
-
-    ellipse_areaC{t} = parse_csv_pupil_coordinates(csvTable, tbytDat(t).faceCam); 
-    fprintf('Frames in trial #%d is parsed for pupil size.\n', t);
+% cropped video files
+croppedVidFiles = dir(fullfile(filePath_cropped_vid, 'behvid*cropped.mp4'));
+if numel(tbytDat) == numel(croppedVidFiles)
+    cropVidNames = {croppedVidFiles.name}; 
+    cropVidTimeTokenC = cellfun(@(a) regexp(a, '(\d{2}_\d{2}_\d{2})', 'tokens'), cropVidNames, 'un', 0); 
+    cropVidTimeC  = cellfun(@(c) hmsToDateTime(c{1}{1}), cropVidTimeTokenC, 'un', 0); 
+    [~, cropVidTimeI] = sort([cropVidTimeC{:}]); % to ensure the cropVid files are in correct timely order
+    cropVidNamesSorted = cropVidNames(cropVidTimeI); 
+    cropVidNamesSorted = cellfun(@(a) fullfile(filePath_cropped_vid, a), cropVidNamesSorted, 'un', 0); 
+    [tbytDat(:).cropVidFile] = deal(cropVidNamesSorted{:}); 
+else
+    warning('The number of cropVid files does not match with the number of trials!') % If some videos are missing each videoes need to be assigned using the trial indices in their names (TO DO)
 end
+disp("Completed organizing the cropped video files!")
 
-
-
-%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    function [ellipse_area, faceCamPt] = parse_csv_pupil_coordinates(csvTable, faceCamPt)
-    % match frame numbers
-    if length(faceCamPt) > size(csvTable, 1)
-        faceCamPt = faceCamPt(1:size(csvTable, 1)); 
-    end
-    
-    ellipse_area = nan(size(csvTable, 1), 1); 
-
-    % fit ellipse frame by frame
-    for fr = 1:size(csvTable)
-        tabFr = csvTable(fr, :); 
-        X = []; 
-        Y = []; 
-        % L coordinates (1)
-        if tabFr.L_like >= 0.5
-            X(end+1) = tabFr.L_x; 
-            Y(end+1) = tabFr.L_y; 
-        end
-        % LD coordinates (2)
-        if tabFr.LD_like >= 0.5
-            X(end+1) = tabFr.LD_x; 
-            Y(end+1) = tabFr.LD_y; 
-        end
-        % D coordinates (3)
-        if tabFr.D_like >= 0.5
-            X(end+1) = tabFr.D_x; 
-            Y(end+1) = tabFr.D_y; 
-        end
-        % DR coordinates (4)
-        if tabFr.DR_like >= 0.5
-            X(end+1) = tabFr.DR_x; 
-            Y(end+1) = tabFr.DR_y; 
-        end
-        % R coordinates (5)
-        if tabFr.R_like >= 0.5
-            X(end+1) = tabFr.R_x; 
-            Y(end+1) = tabFr.R_y; 
-        end
-        % RV coordinates (6)
-        if tabFr.RV_like >= 0.5
-            X(end+1) = tabFr.RV_x; 
-            Y(end+1) = tabFr.RV_y; 
-        end
-        % V coordinates (7)
-        if tabFr.V_like >= 0.5
-            X(end+1) = tabFr.V_x; 
-            Y(end+1) = tabFr.V_y; 
-        end
-        % VL coordinates (8)
-        if tabFr.VL_like >= 0.5
-            X(end+1) = tabFr.VL_x; 
-            Y(end+1) = tabFr.VL_y; 
-        end
-
-        ellipse_fit = fit_ellipse(X, Y); 
-        ellipse_area(fr) = ellipse_fr.long_axis * ellipse_fr.short_axis * pi; 
-
-    end
-    
-    
-
-
-    end
-
-
-
-
-
+save(fullfile(tbytDatPath{1}), 'tbytDat')
 
 
 end
--- mesomapping/pupillometry/parse_csv_pupil_coordinates.m
+++ mesomapping/pupillometry/parse_csv_pupil_coordinates.m
@@ -1,58 +1,65 @@
-    function [ellipse_area, faceCamPt] = parse_csv_pupil_coordinates(csvTable, faceCamPt)
+function [ellipse_area, faceCamPt, ellipse_fit, eyeOpenPer] = parse_csv_pupil_coordinates(csvTable, faceCamPt, height)
     % match frame numbers
     if length(faceCamPt) > size(csvTable, 1)
         faceCamPt = faceCamPt(1:size(csvTable, 1)); 
     end
     
     ellipse_area = nan(size(csvTable, 1), 1); 
-
+    eyeOpenPer = nan(size(csvTable, 1), 1); 
+    
     % fit ellipse frame by frame
-    for fr = 1:size(csvTable)
+    for fr = 1:size(csvTable, 1)
         tabFr = csvTable(fr, :); 
         X = []; 
         Y = []; 
         % L coordinates (1)
         if tabFr.L_like >= 0.5
             X(end+1) = tabFr.L_x; 
-            Y(end+1) = tabFr.L_y; 
+            Y(end+1) = height-tabFr.L_y; 
         end
         % LD coordinates (2)
         if tabFr.LD_like >= 0.5
             X(end+1) = tabFr.LD_x; 
-            Y(end+1) = tabFr.LD_y; 
+            Y(end+1) = height-tabFr.LD_y; 
         end
         % D coordinates (3)
         if tabFr.D_like >= 0.5
             X(end+1) = tabFr.D_x; 
-            Y(end+1) = tabFr.D_y; 
+            Y(end+1) = height-tabFr.D_y; 
         end
         % DR coordinates (4)
         if tabFr.DR_like >= 0.5
             X(end+1) = tabFr.DR_x; 
-            Y(end+1) = tabFr.DR_y; 
+            Y(end+1) = height-tabFr.DR_y; 
         end
         % R coordinates (5)
         if tabFr.R_like >= 0.5
             X(end+1) = tabFr.R_x; 
-            Y(end+1) = tabFr.R_y; 
+            Y(end+1) = height-tabFr.R_y; 
         end
         % RV coordinates (6)
         if tabFr.RV_like >= 0.5
             X(end+1) = tabFr.RV_x; 
-            Y(end+1) = tabFr.RV_y; 
+            Y(end+1) = height-tabFr.RV_y; 
         end
         % V coordinates (7)
         if tabFr.V_like >= 0.5
             X(end+1) = tabFr.V_x; 
-            Y(end+1) = tabFr.V_y; 
+            Y(end+1) = height-tabFr.V_y; 
         end
         % VL coordinates (8)
         if tabFr.VL_like >= 0.5
             X(end+1) = tabFr.VL_x; 
-            Y(end+1) = tabFr.VL_y; 
+            Y(end+1) = height-tabFr.VL_y; 
         end
+        
+        eyeOpenPer(fr) = length(X)/8; 
 
-        ellipse_fit = fit_ellipse(X, Y); 
-        ellipse_area(fr) = ellipse_fit.long_axis * ellipse_fit.short_axis * pi; 
-
+        if length(X) > 5 && length(Y) > 5
+            ellipse_fit = fit_ellipse(X, Y); 
+            %ellipse_fit_status{fr} = ellipse_fit.status; 
+            ellipse_area(fr) = ellipse_fit.long_axis * ellipse_fit.short_axis * pi; 
+        end
+    end
+    
     end
